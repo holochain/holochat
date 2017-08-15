a8cfb24538a520f6607a4cf483d87ab0b93f4dbf
Joel U <joelulahanna@gmail.com>
2017-08-15 09:54:35 -0700

    updates to identity

--- dna/identity/identity.js
+++ dna/identity/identity.js
@@ -1,26 +1,34 @@
 function genesis(){return true}
 
 function bridgeGenesis(){
+  debug("bridgeGenesis Called")
   appDNAHash=getDpkiDNA()
-  bridge(appDNAHash, "dpkiLib", "registerDpkiTo", App.Agent.Hash)
+  data=bridge(appDNAHash, "dpkiLib", "registerDpkiTo", App.Agent.Hash)
+  debug("data = "+data)
   return true
 }
 
 function registerDpkiKeyTo(){
   appDNAHash=getDpkiDNA();
+  debug("registerDpkiKeyTo = "+appDNAHash)
   data = bridge(appDNAHash, "dpkiLib", "registerDpkiKeyTo", App.Agent.Hash)
+  debug("data = "+data)
   return data
 }
 
 function hasRegisteredKey(app_agent_id){
   appDNAHash=getDpkiDNA();
+  debug("hasRegisteredKey = "+appDNAHash)
   data = bridge(appDNAHash, "dpkiLib", "hasRegisteredKey",app_agent_id)
+  debug("data = "+data)
   return data
 }
 
 function getUserDetails(app_agent_id){
   appDNAHash=getDpkiDNA();
+  debug("getUserDetails = "+appDNAHash)
   data = bridge(appDNAHash, "dpkiLib", "getUserDetails", app_agent_id)
+  debug("data = "+data)
   return data
 }
 
@@ -29,6 +37,6 @@ function getUserDetails(app_agent_id){
 // DPKI DNA Hash
 ////////////////
 function getDpkiDNA(){
-  appDNAHash=QBAIDFYBVKDYB$RHFOISBD245243ASFC
+  appDNAHash="QmXAn9Mr4fgStoJysri7UnttfaMqtBT5CW7FR82cZcaYRx"
   return appDNAHash
 }
