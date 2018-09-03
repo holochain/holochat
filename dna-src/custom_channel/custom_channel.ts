286fc60c91a2eaa29aee0f880fe325bd5c92b86a
Willem Olding <willemolding@Julia-Oldings-MacBook-2.local>
2018-09-03 10:39:01 +1000

    renames rooms to channels and adds jsdoc to all public functions

--- dna-src/custom_channel/custom_channel.ts
+++ dna-src/custom_channel/custom_channel.ts
@@ -14,21 +14,25 @@ type UUID = string;
 =            Public Zome Functions            =
 =============================================*/
 
-
-function createCustomRoom(members: holochain.Hash[]): UUID {
+/**
+ * Creates a channel with members
+ * @param  {holochain.Hash[]} - Array of public key hashes of members to add to channel
+ * @return {UUID} - Channel UUID
+ */
+function createCustomChannel(members: holochain.Hash[]): UUID {
   members.push(App.Key.Hash)
   let uuid: string = uuidGenerator();
-  var custom_room_details: any = {
+  var custom_channel_details: any = {
     name: uuid
   }
-  debug("Your Room UUID: " + uuid);
-  let uuid_hash = commit("custom_room_uuid", uuid);
+  debug("Your Channel UUID: " + uuid);
+  let uuid_hash = commit("custom_channel_uuid", uuid);
   //debug("uuid_hash: " + uuid_hash);
-  commit("custom_room_link", { Links: [{ Base: App.DNA.Hash, Link: uuid_hash, Tag: "room_uuid" }] });
+  commit("custom_channel_link", { Links: [{ Base: App.DNA.Hash, Link: uuid_hash, Tag: "channel_uuid" }] });
 
-  let details_hash = commit("custom_room_details", custom_room_details);
+  let details_hash = commit("custom_channel_details", custom_channel_details);
   //debug("details_hash: " + details_hash);
-  commit("custom_room_link", { Links: [{ Base: uuid_hash, Link: details_hash, Tag: "room_details" }] });
+  commit("custom_channel_link", { Links: [{ Base: uuid_hash, Link: details_hash, Tag: "channel_details" }] });
 
   addMembers(uuid, members);
 
@@ -37,43 +41,55 @@ function createCustomRoom(members: holochain.Hash[]): UUID {
 
 
 
-//TODO : Test for non creator of the room adding a member in the room
-//Adds Members to a room using the UUID of the room
+//TODO : Test for non creator of the channel adding a member in the channel
+/**
+ * Add members to an existing channel
+ * @param  {UUID} - Channel UUID
+ * @param  {holochain.Hash[]} - Array of members to add
+ * @return {boolean} - Returns true if successful otherwise returns an error
+ */
 function addMembers(uuid: UUID, members: holochain.Hash[]): boolean | holochain.HolochainError {
-  let uuid_hash: string = makeHash("custom_room_uuid", uuid);
+  let uuid_hash: string = makeHash("custom_channel_uuid", uuid);
   members.forEach((member) => {
     try {
-      commit("room_to_member_link", { Links: [{ Base: uuid_hash, Link: member, Tag: "room_members" }] });
+      commit("channel_to_member_link", { Links: [{ Base: uuid_hash, Link: member, Tag: "channel_members" }] });
     } catch (e) {
       debug(e)
       return e
     }
-    commit("member_to_room_link", { Links: [{ Base: member, Link: uuid_hash, Tag: "my_rooms" }] });
+    commit("member_to_channel_link", { Links: [{ Base: member, Link: uuid_hash, Tag: "my_channels" }] });
   });
   return true;
 }
 
 
 
-
-function getMyRooms(): Array<any> | holochain.HolochainError {
-  let my_rooms: any;
+/**
+ * Get the channels this user is a member of
+ * @return {Array<any>} - Array of channel specs for channels this user is a member of
+ */
+function getMyChannels(): Array<any> | holochain.HolochainError {
+  let my_channels: any;
   try {
-    my_rooms = getLinks(App.Key.Hash, "my_rooms", { Load: true });
+    my_channels = getLinks(App.Key.Hash, "my_channels", { Load: true });
   } catch (e) {
     return e;
   }
-  debug("My Room Chats : " + JSON.stringify(my_rooms));
-  return my_rooms.map((room) => { return room.Entry });
+  debug("My Channel Chats : " + JSON.stringify(my_channels));
+  return my_channels.map((channel) => { return channel.Entry });
 }
 
 
 
-// Call to get all the member for a perticual UUID
-function getMembers(uuid: UUID): string[] {
+/**
+ * Retrieve the members of a channel given its UUID
+ * @param  {UUID} - Channel UUID
+ * @return {string[]} - Array of key hashes of members in channel
+ */
+function getMembers(uuid: UUID): holochain.Hash[] | holochain.HolochainError {
   let members: any;
   try {
-    members = getLinks(makeHash("custom_room_uuid", uuid), "room_members", { Load: true });
+    members = getLinks(makeHash("custom_channel_uuid", uuid), "channel_members", { Load: true });
   } catch (e) {
     return e;
   }
@@ -81,19 +97,27 @@ function getMembers(uuid: UUID): string[] {
   return members;
 }
 
-// Call to get details for a perticual UUID
-function getRoomDetails(uuid: UUID): string[] {
+/**
+ * Get the details of a channel given its UUID
+ * @param  {UUID} - Channel UUID
+ * @return {string[]} - TODO: Write spec for channel details
+ */
+function getChannelDetails(uuid: UUID): string[] {
   let details: any;
   try {
-    details = getLinks(makeHash("custom_room_uuid", uuid), "room_details", { Load: true });
+    details = getLinks(makeHash("custom_channel_uuid", uuid), "channel_details", { Load: true });
   } catch (e) {
     return e;
   }
-  debug("Room Details for " + uuid + ": " + JSON.stringify(details));
+  debug("Channel Details for " + uuid + ": " + JSON.stringify(details));
   return details;
 }
 
-//@param payload:{uuid:string,message:any}
+/**
+ * Post a message to a channel
+ * @param  {message} - message object to post
+ * @return {holochain.Hash} - Returns the hash of the message if successful or an error
+ */
 function postMessage(payload: message): holochain.Hash | holochain.HolochainError {
   debug(payload)
   payload.message.timestamp = new Date();
@@ -102,18 +126,24 @@ function postMessage(payload: message): holochain.Hash | holochain.HolochainErro
 
   let hash: holochain.Hash;
   try {
-    hash = commit("cr_message", payload.message);
-    commit("cr_message_link", { Links: [{ Base: makeHash("custom_room_uuid", payload.uuid), Link: hash, Tag: "messages" }] });
+    hash = commit("message", payload.message);
+    commit("message_link", { Links: [{ Base: makeHash("custom_channel_uuid", payload.uuid), Link: hash, Tag: "messages" }] });
   } catch (e) {
     return e;
   }
   return hash;
 }
 
+
+/**
+ * Get all the messages from a channel given its UUID
+ * @param  {UUID} - Channel UUID
+ * @return {Array<holochain.GetLinksResponse>} - Array of messages
+ */
 function getMessages(uuid: UUID): Array<holochain.GetLinksResponse> | holochain.HolochainError {
   let messages;
   try {
-    let messages = getLinks(makeHash("custom_room_uuid", uuid), "messages", { Load: true });
+    let messages = getLinks(makeHash("custom_channel_uuid", uuid), "messages", { Load: true });
     debug("Messages : " + JSON.stringify(messages))
     return messages;
   } catch (e) {
@@ -123,12 +153,16 @@ function getMessages(uuid: UUID): Array<holochain.GetLinksResponse> | holochain.
 
 }
 
-//@param payload:{new_message:"",old_hash:""}
-function updateMessage(payload: updateMessageType): holochain.Hash {
+/**
+ * Updates an already posted message
+ * @param  {updateMessageType} - Message update object
+ * @return {holochain.Hash} - Returns the hash of the new message or an error
+ */
+function updateMessage(payload: updateMessageType): holochain.Hash | holochain.HolochainError {
   debug(payload);
   payload.new_message.timestamp = new Date();
   payload.new_message.author = App.Key.Hash;
-  let hash: holochain.Hash = update("cr_message", payload.new_message, payload.old_hash);
+  let hash: holochain.Hash = update("message", payload.new_message, payload.old_hash);
   return hash;
 }
 
@@ -169,24 +203,24 @@ function genesis() {
 // Check if the pub_hash is a member of the
 function isValidAdmin(base_hash: string, entry_source: string): boolean {
   debug("Checking if Agent is an Admin..");
-  //Checking if the Creator is trying to add people to the room
+  //Checking if the Creator is trying to add people to the channel
   let source: any;
   try {
     source = get(base_hash, { GetMask: HC.GetMask.Sources });
   } catch (e) {
     return false;
   }
-  //Added the creater of the room as a member of the room
+  //Added the creater of the channel as a member of the channel
   if (JSON.stringify(source) === JSON.stringify(entry_source)) {
-    //debug("Adding Room Creator as a member of the room")
+    //debug("Adding Channel Creator as a member of the channel")
     return true;
   }
-  //Checking to see if members are trying to add new members to the room
+  //Checking to see if members are trying to add new members to the channel
   let members: any;
   try {
-    members = getLinks(base_hash, "room_members", { Load: true });
+    members = getLinks(base_hash, "channel_members", { Load: true });
   } catch (e) {
-    debug("Rooms Dosnt Exist " + e);
+    debug("Channels Dosnt Exist " + e);
     return false;
   }
   let access: boolean = members.some((member) => {
@@ -214,20 +248,20 @@ function validateCommit(entryName: any, entry: any, header: any, pkg: any, sourc
 
 function validate(entryName: any, entry: any, header: any, pkg: any, sources: any): boolean {
   switch (entryName) {
-    case "custom_room_uuid":
+    case "custom_channel_uuid":
       return true;
-    case "custom_room_details":
+    case "custom_channel_details":
       return true;
-    case "custom_room_link":
+    case "custom_channel_link":
       return true;
-    case "room_to_member_link":
+    case "channel_to_member_link":
       return isValidAdmin(entry.Links[0].Base, sources);
-    case "member_to_room_link":
+    case "member_to_channel_link":
       //isValidAdmin(entry);
       return true;
-    case "cr_message":
+    case "message":
       return true;
-    case "cr_message_link":
+    case "message_link":
       return true;
     default:
       return false;
@@ -241,7 +275,7 @@ function validatePut(entryName: any, entry: any, header: any, pkg: any, sources:
 function validateMod(entryName: any, entry: any, header: any, replaces: any, pkg: any, sources: any): boolean {
   debug("entry_type:" + entryName + "entry" + JSON.stringify(entry) + "header" + JSON.stringify(header) + "replaces: " + replaces + "PKG: " + JSON.stringify(pkg) + "sources" + sources);
   switch (entryName) {
-    case "cr_message":
+    case "message":
       return isValidModifier(replaces, sources);
     default:
       return false;
@@ -255,13 +289,13 @@ function validateDel(entryName: any, hash: any, pkg: any, sources: any): boolean
 function validateLink(entryName: any, baseHash: any, links: any, pkg: any, sources: any): boolean {
   //debug("entryName: "+entryName+" baseHash: "+ baseHash+" links: "+ links+" sources: "+ sources);
   switch (entryName) {
-    case "custom_room_link":
+    case "custom_channel_link":
       return true;
-    case "room_to_member_link":
+    case "channel_to_member_link":
       return true;
-    case "member_to_room_link":
+    case "member_to_channel_link":
       return true;
-    case "cr_message_link":
+    case "message_link":
       return true;
     default:
       return false;
